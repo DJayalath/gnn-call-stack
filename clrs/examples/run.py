f9f61c0be178941c8436f8951a992799a22af92f
jonasjuerss <jonas.juerss@tum.de>
2023-02-23 19:38:06 +0000

    Small fixes

--- clrs/examples/run.py
+++ clrs/examples/run.py
@@ -126,7 +126,7 @@ flags.DEFINE_enum('stack_pooling_fun', 'max',
 flags.DEFINE_integer('num_hiddens_for_stack', 64,
                     'How many of the node embedding entries to use for generating the stack embedding.')
 
-flags.DEFINE_enum('callstack_type', 'none', ['none', 'graph_level', 'node_level'],
+flags.DEFINE_enum('callstack_type', 'graphlevel', ['none', 'graphlevel', 'nodelevel'],
                      'The type of callstack to use. This only works if the specification has a suitable hint called '
                      'stack_op.')
 flags.DEFINE_boolean('checkpoint_wandb', True,
@@ -382,7 +382,7 @@ def create_samplers(rng, train_lengths: List[int]):
 def main(unused_argv):
   global FLAGS
   # FLAGS.use_callstack = FLAGS.callstack_type != 'none' # for backwards compatibility of logs
-  FLAGS = utils.init(FLAGS)
+  FLAGS, unpackable_flags = utils.init(FLAGS)
   if FLAGS.hint_mode == 'encoded_decoded':
     encode_hints = True
     decode_hints = True
@@ -428,7 +428,7 @@ def main(unused_argv):
       hint_repred_mode=FLAGS.hint_repred_mode,
       checkpoint_wandb=FLAGS.checkpoint_wandb,
       nb_msg_passing_steps=FLAGS.nb_msg_passing_steps,
-      callstack=callstack_from_name(**FLAGS.flag_values_dict())
+      callstack=callstack_from_name(**unpackable_flags)
       )
 
   eval_model = clrs.models.BaselineModel(
--- gnn_call_stack/utils.py
+++ gnn_call_stack/utils.py
@@ -1,9 +1,11 @@
+from argparse import Namespace
+
 import wandb
 def init(args):
   if args.use_wandb:
     wandb.init(project="gnn-call-stack", entity="camb-mphil", config=args)
-    return wandb.config
-  return args
+    return wandb.config, wandb.config
+  return args, args.flag_values_dict() # To make unpackable
 
 def log(*args, **kwargs):
   if wandb.run is not None:
