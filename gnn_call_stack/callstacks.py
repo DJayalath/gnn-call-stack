4282ae91a41e943a372ddd14e5cc8a26a97e6427
jonasjuerss <jonas.juerss@tum.de>
2023-02-23 17:45:11 +0000

    Fixed graph-level callstack and implemented node-level callstack

--- gnn_call_stack/callstacks.py
+++ gnn_call_stack/callstacks.py
@@ -42,6 +42,13 @@ class NoneCallstack(Callstack):
   def step(self, mp_state, hint_preds, hiddens):
     return None, None
   def add_to_features(self, top_stack, node_fts, edge_fts, graph_fts):
+    """
+    :param top_stack: as returned by get_top()
+    :param node_fts: [batch_size, num_nodes, hidden_dim]
+    :param edge_fts: [batch_size, num_nodes, num_nodes, hidden_dim]
+    :param graph_fts: [batch_size, hidden_dim]
+    :returns: modified (node_fts, edge_fts, graph_fts)
+    """
     return node_fts, edge_fts, graph_fts
 
 
@@ -53,14 +60,14 @@ class GraphLevelCallstack(Callstack):
     self.stack_pooling_fun = getattr(jnp, stack_pooling_fun)
 
   def initial_values(self, batch_size: int, num_steps: int, num_nodes: int):
-    stack = jnp.zeros((batch_size, num_steps, self.num_hiddens_for_stack))
+    stack = jnp.zeros((batch_size, num_steps + 1, self.num_hiddens_for_stack))
     stack_pointers = jnp.zeros((batch_size,), dtype=int)
     return stack, stack_pointers
   def get_top(self, mp_state: _MessagePassingScanState):
     # mp_state.stack: [batch_size, max_stack_size/num_time_steps, num_hiddens_for_stack]
     # mp_state.stack_pointers: [batch_size]
-    return mp_state.stack.reshape(-1, mp_state.stack.shape[-1])[
-           mp_state.stack_pointers + 1 + 3 * jnp.arange(mp_state.stack.shape[0]), :]
+    stack, stack_pointers = mp_state.stack, mp_state.stack_pointers
+    return stack.reshape(-1, stack.shape[-1])[stack_pointers + stack.shape[1] * jnp.arange(stack.shape[0]), :]
 
   def step(self, mp_state, hint_preds, hiddens):
     # [batch_size, max_stack_size/num_time_steps, num_hiddens_for_stack]
@@ -81,18 +88,58 @@ class GraphLevelCallstack(Callstack):
     #                            stack.reshape(-1, stack.shape[-1])[stack_pointers + 3 * jnp.arange(stack.shape[0]), :])
     # Overwrite the next stack element with the calculated one independent of the actual operation. The operation is
     # taken into account by only moving the pointers forward where we actually pushed
-    stack = stack.reshape(-1, stack.shape[-1]).at[stack_pointers + 1 + 3 * jnp.arange(stack.shape[0]), :] \
+    stack = stack.reshape(-1, stack.shape[-1]).at[stack_pointers + 1 + stack.shape[1] * jnp.arange(stack.shape[0]), :] \
       .set(new_stack_vals) \
       .reshape(mp_state.stack.shape)
     stack_pointers = jnp.maximum(stack_pointers + stack_ops - 1, 0)
     return stack, stack_pointers
 
   def add_to_features(self, top_stack, node_fts, edge_fts, graph_fts):
-    graph_fts = jnp.concatenate((graph_fts, top_stack), axis=1)
+    graph_fts = jnp.concatenate((graph_fts, top_stack), axis=-1)
     return node_fts, edge_fts, graph_fts
 
+class NodeLevelCallstack(Callstack):
 
-__all__ = [NoneCallstack, GraphLevelCallstack]
+  def __init__(self, num_hiddens_for_stack: int, **kwargs):
+    super().__init__(**kwargs)
+    self.num_hiddens_for_stack = num_hiddens_for_stack
+
+  def initial_values(self, batch_size: int, num_steps: int, num_nodes: int):
+    stack = jnp.zeros((batch_size, num_steps + 1, num_nodes, self.num_hiddens_for_stack))
+    stack_pointers = jnp.zeros((batch_size,), dtype=int)
+    return stack, stack_pointers
+  def get_top(self, mp_state: _MessagePassingScanState):
+    # mp_state.stack: [batch_size, max_stack_size/num_time_steps, num_nodes, num_hiddens_for_stack]
+    # mp_state.stack_pointers: [batch_size]
+    stack, stack_pointers = mp_state.stack, mp_state.stack_pointers
+    return stack.reshape(-1, stack.shape[2], stack.shape[3])[
+           stack_pointers + jnp.arange(stack.shape[0]) * stack.shape[1], : , :]
+
+  def step(self, mp_state, hint_preds, hiddens):
+    # [batch_size, max_stack_size/num_time_steps, num_nodes, num_hiddens_for_stack]
+    stack = mp_state.stack
+    # [batch_size]
+    stack_pointers = mp_state.stack_pointers
+
+    # [batch_size] containing [0 (pop), 1 (noop) or 2 (push)]
+    stack_ops = jnp.argmax(hint_preds["stack_op"], axis=-1)
+    # [batch_size, num_nodes, num_hiddens_for_stack] values that would be pushed to the stack in case of "push"
+    new_stack_vals = hiddens[:, :, :, :self.num_hiddens_for_stack]
+
+    # Overwrite the next stack element with the calculated one independent of the actual operation. The operation is
+    # taken into account by only moving the pointers forward where we actually pushed
+    stack = stack.reshape(-1, stack.shape[2], stack.shape[3])\
+              .at[stack_pointers + 1 + stack.shape[1] * jnp.arange(stack.shape[0]), :, :]\
+      .set(new_stack_vals) \
+      .reshape(mp_state.stack.shape)
+    stack_pointers = jnp.maximum(stack_pointers + stack_ops - 1, 0)
+    return stack, stack_pointers
+
+  def add_to_features(self, top_stack, node_fts, edge_fts, graph_fts):
+    graph_fts = jnp.concatenate((node_fts, top_stack), axis=-1)
+    return node_fts, edge_fts, graph_fts
+
+__all__ = [NoneCallstack, GraphLevelCallstack, NodeLevelCallstack]
 def callstack_from_name(callstack_type: str, **kwargs) -> Callstack:
   for c in __all__:
     if c.__name__.lower()[:-9] == callstack_type.lower():
